c6a8c3e144f3c624e6c91c022eda3137add703a2
Nava Wu <69130725+ns-wu@users.noreply.github.com>
2024-11-13 11:52:01 -0500

    changed UUID to reflect heart rate example

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function requestBluetoothDevice() {
   log('Requesting bluetooth device...');
 
   return navigator.bluetooth.requestDevice({
-    filters: [{services: [0xFFE0]}],
+    filters: [{services: [0x180D]}],
   }).
       then(device => {
         log('"' + device.name + '" bluetooth device selected');
@@ -80,12 +80,12 @@ function connectDeviceAndCacheCharacteristic(device) {
       then(server => {
         log('GATT server connected, getting service...');
 
-        return server.getPrimaryService(0xFFE0);
+        return server.getPrimaryService(0x180D);
       }).
       then(service => {
         log('Service found, getting characteristic...');
 
-        return service.getCharacteristic(0xFFE1);
+        return service.getCharacteristic(0x2A37);
       }).
       then(characteristic => {
         log('Characteristic found');
